6ba45bc0a42e97d524174ac6a97d8d8cd14e4e44
pat <73788322+pat-3@users.noreply.github.com>
2022-03-04 08:21:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# Resources to use:
+#     -yfinance https://pypi.org/project/yfinance/
+#         -https://towardsdatascience.com/the-easiest-way-to-pull-stock-data-into-your-python-program-yfinance-82c304ae35dc
+#     -tweepy https://realpython.com/twitter-bot-python-tweepy/
+#         -https://www.geeksforgeeks.org/how-to-make-a-twitter-bot-in-python/
 
