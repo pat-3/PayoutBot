0de1ccf952c5cdbd329cee4137639dfe6fd24e82
pat <73788322+pat-3@users.noreply.github.com>
2022-03-28 08:29:47 -0400

    Update scraper_in_progress.py

--- scraper_in_progress.py
+++ scraper_in_progress.py
@@ -4,31 +4,29 @@ import numpy
 import schedule
 import time
 
-#Gets the current evaluation for the stock and assigns it to a variable
-def CurrentValue():
-    Stock1 = print(numpy.array2string(stock_info.get_live_price('HACK')))
-    Stock2 = print(numpy.array2string(stock_info.get_live_price('GXC')))
-    Stock3 = print(numpy.array2string(stock_info.get_live_price('VOX')))
-    Stock4 = print(numpy.array2string(stock_info.get_live_price('ITA')))
-    Stock5 = print(numpy.array2string(stock_info.get_live_price('GLTR')))
-    Stock6 = print(numpy.array2string(stock_info.get_live_price('ICLN')))
-    Stock7 = print(numpy.array2string(stock_info.get_live_price('AOR')))
-    Stock8 = print(numpy.array2string(stock_info.get_live_price('GLD')))
-CurrentValue()
+#Gets the current dollar value for the stock and assigns it to a variable
+Stock1 = numpy.array2string(stock_info.get_live_price('HACK'))
+Stock2 = numpy.array2string(stock_info.get_live_price('GXC'))
+Stock3 = numpy.array2string(stock_info.get_live_price('VOX'))
+Stock4 = numpy.array2string(stock_info.get_live_price('ITA'))
+Stock5 = numpy.array2string(stock_info.get_live_price('GLTR'))
+Stock6 = numpy.array2string(stock_info.get_live_price('ICLN'))
+Stock7 = numpy.array2string(stock_info.get_live_price('AOR'))
+Stock8 = numpy.array2string(stock_info.get_live_price('GLD'))
 
 
 #Function for getting current price
 def CurrentPrice():
     if CurrentPrice:
-        #Gets current price from yahoo_fin and outputs it in terminal, just change the ticker and ticker name
-        print("Current price of: HACK $" + Stock1)
-        print("Current price of ticker: GXC" + " $" +  {Stock2})
-        print("Current price of ticker: VOX" + " $" + {Stock3})
-        print("Current price of ticker: ITA" + " $" + {Stock4})
-        print("Current price of ticker: GLTR" + " $" + {Stock5})
-        print("Current price of ticker: ICLN" + " $" + {Stock6})
-        print("Current price of ticker: AOR" + " $" + {Stock7})
-        print("Current price of ticker: GLD" + " $" + {Stock8})
+        #Outputs the value of the the stock variable and puts in terminal so its usable
+        print("Current price of HACK: $" + str(Stock1))
+        print("Current price of ticker GXC: $" +  str(Stock2))
+        print("Current price of ticker VOX: $" + str(Stock3))
+        print("Current price of ticker ITA: $" + str(Stock4))
+        print("Current price of ticker GLTR: $" + str(Stock5))
+        print("Current price of ticker ICLN: $" + str(Stock6))
+        print("Current price of ticker AOR: $" + str(Stock7))
+        print("Current price of ticker GLD: $" + str(Stock8))
     else:
         print('panic.')
 CurrentPrice()
